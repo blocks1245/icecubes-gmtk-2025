2dc78633f55dc3f8b7f80fb9319d3758a03cdc2e
Cygnus the rock <cygnustherock@gmail.com>
2025-07-31 13:49:56 +0200

    Added a better way to count total rooms

--- scripts/gameManager.gd
+++ scripts/gameManager.gd
@@ -1,7 +1,7 @@
 extends Node
 
 @onready var score = 0
-@onready var rooms = 3
+@onready var rooms = len(DirAccess.get_files_at("res://scenes/rooms/"))
 
 
 func addScore(): #adds to the current score
@@ -13,5 +13,6 @@ func resetScore(): # resets the current score
 func getScore(): # returns the score
 	return score
 
-func getRooms():# returns the number of roomsaaa
+func getRooms():# returns the number of rooms
+	print(rooms)
 	return rooms
